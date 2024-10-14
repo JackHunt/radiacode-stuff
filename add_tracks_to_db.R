8604ab7c61010c40c87b70c41ffd0eaf78005a7e
Jack Hunt <jackmileshunt@gmail.com>
2024-10-14 19:43:06 +0100

    Update add_tracks_to_db.R

--- add_tracks_to_db.R
+++ add_tracks_to_db.R
@@ -1,9 +1,37 @@
+library(DBI)
 library(optparse)
 
 source("db_helpers.R")
 
+add_devices <- function(devices, con) {
+  load_sql_function("add_device", con)
+
+  lapply(devices, function(device) {
+    dbGetQuery(con, sprintf("SELECT add_device('%s');", device))
+  })
+}
+
+add_session <- function(session_data, devices, con) {
+  load_sql_function("add_session", con)
+
+  dbGetQuery(con, sprintf(
+    "SELECT add_session('%s', %s, %s, %s);",
+    session_data$title,
+    session_data$start,
+    session_data$sv,
+    session_data$devices
+  ))
+}
+
 process_tracks <- function(tracks, con) {
-  #
+  lapply(tracks, function(track) {
+    session_data <- track$session_data
+    markers <- track$markers
+
+    device_keys <- add_devices(session_data$devices, con)
+    session_key <- add_session(session_data, device_keys, con)
+    print((device_keys[[1]]$add_device))
+  })
 }
 
 if (!interactive()) {
@@ -28,3 +56,7 @@ if (!interactive()) {
   con <- get_connection()
   process_tracks(tracks, con)
 }
+
+tracks <- readRDS("tracks.rds")
+con <- get_connection()
+process_tracks(tracks, con)
