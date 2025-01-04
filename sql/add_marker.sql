d9f90aaefabc2a967d01f153d20f8054cb66905f
Jack Hunt <jackmileshunt@gmail.com>
2025-01-04 17:34:28 +0000

    Update add_marker.sql

--- sql/add_marker.sql
+++ sql/add_marker.sql
@@ -5,7 +5,7 @@ RETURNS integer AS $$
     BEGIN
     SELECT id INTO marker_key FROM sessions WHERE marker_time = p_time;
     IF marker_key is NULL THEN
-      INSERT INTO markers () VALUES ()
+      INSERT INTO markers (latitude, longitude, marker_time, count_rate, dose_rate) VALUES (p_lat, p_lon, p_time, p_count_rate, p_dose_rate)
       RETURNING id INTO marker_key
     END IF;
 END;
